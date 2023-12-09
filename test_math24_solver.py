fab1c82916eb04f9b5c22877379931adeb1d13a1
Atticus Russell <atticusrussell@gmail.com>
2023-12-09 05:56:12 +0000

    swap to pytest

--- test_math24_solver.py
+++ test_math24_solver.py
@@ -1,19 +1,14 @@
-import unittest
 from solve_math24 import solve_math24
 from fractions import Fraction
 
-class TestMath24Solver(unittest.TestCase):
-    def test_basic_solution(self):
-        self.assertEqual(solve_math24([3, 3, 8, 8]), '8 / (3 - (8 / 3))')
+def test_basic_solution():
+    assert solve_math24([3, 3, 8, 8]) == '8 / (3 - (8 / 3))'
 
-    def test_no_solution(self):
-        self.assertEqual(solve_math24([1, 1, 1, 1]), 'No solution possible')
+def test_no_solution():
+    assert solve_math24([1, 1, 1, 1]) == 'No solution possible'
 
-    def test_decimal_input(self):
-        self.assertEqual(solve_math24([1.5, 6, 8, 9]), '8 * (3/2 + (9 / 6))')
+def test_decimal_input():
+    assert solve_math24([1.5, 6, 8, 9]) == '8 * (1.5 + (9 / 6))'
 
-    def test_fraction_input(self):
-        self.assertEqual(solve_math24([3/2, 6, 8, 9]), '8 * (3/2 + (9 / 6))')
-
-if __name__ == '__main__':
-    unittest.main()
+def test_fraction_input():
+    assert solve_math24(['3/2', 6, 8, 9]) == '8 * (3/2 + (9 / 6))'
