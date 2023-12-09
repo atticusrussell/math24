404ebea4ff1db387e449338d28961cf25e7422d1
Atticus Russell <atticusrussell@gmail.com>
2023-12-09 05:56:48 +0000

    input decimals result in output decimals

--- solve_math24.py
+++ solve_math24.py
@@ -1,11 +1,26 @@
 import itertools
 import operator
-from fractions import Fraction
 import sys
+from fractions import Fraction
 
 def solve_math24(numbers):
-    # Convert inputs to fractions to handle decimals and fractions
-    numbers = [Fraction(str(n)) for n in numbers]
+    # Convert numbers to Fractions if not floats
+    processed_numbers = []
+    for n in numbers:        
+        if isinstance(n, str):
+            if '.' in n:
+                processed_numbers.append(float(n))
+            elif '/' in n:
+                processed_numbers.append(Fraction(n))
+            else:
+                processed_numbers.append(int(n))
+        elif isinstance(n, float):
+            processed_numbers.append(n)
+        else:
+            # ints can be converted to Fractions over 1
+            processed_numbers.append(Fraction(n)) 
+
+    numbers = processed_numbers
 
     # Define operations
     operations = [operator.add, operator.sub, operator.mul, operator.truediv]
@@ -29,9 +44,27 @@ def solve_math24(numbers):
 def format_solution(nums, ops, pattern):
     op_symbols = {operator.add: '+', operator.sub: '-', operator.mul: '*', operator.truediv: '/'}
     symbols = [op_symbols[op] for op in ops]
-    nums = [str(num) for num in nums]
-    return pattern.replace('a', nums[0]).replace('b', nums[1]).replace('c', nums[2]).replace('d', nums[3])\
-                  .replace('o', symbols[0], 1).replace('o', symbols[1], 1).replace('o', symbols[2], 1)
+
+    # Format numbers based on type (Fraction, float, or int)
+    formatted_nums = []
+    for num in nums:
+        if isinstance(num, float):
+            formatted_nums.append(str(num))
+        elif isinstance(num, Fraction):
+            if num.denominator == 1:
+                formatted_nums.append(str(num.numerator))  # Integer representation
+            else:
+                formatted_nums.append(str(num))  # Fraction representation
+        else:
+            formatted_nums.append(str(num))
+
+    return pattern.replace('a', formatted_nums[0])\
+                  .replace('b', formatted_nums[1])\
+                  .replace('c', formatted_nums[2])\
+                  .replace('d', formatted_nums[3])\
+                  .replace('o', symbols[0], 1)\
+                  .replace('o', symbols[1], 1)\
+                  .replace('o', symbols[2], 1)
 
 def main():
     # Check if the correct number of arguments are provided
@@ -40,7 +73,7 @@ def main():
         sys.exit(1)
 
     # Parse command line arguments
-    numbers = [Fraction(arg) for arg in sys.argv[1:]]
+    numbers = sys.argv[1:]
 
     # Solve and print the solution
     solution = solve_math24(numbers)
