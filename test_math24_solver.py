921a0b3da0327a264899d6dc096f5d2b09f5dc19
Atticus Russell <atticusrussell@gmail.com>
2023-12-09 01:43:57 +0000

    basic unit test start

---
+++ test_math24_solver.py
@@ -0,0 +1,16 @@
+import unittest
+from solve_math24 import solve_math24
+from fractions import Fraction
+
+class TestMath24Solver(unittest.TestCase):
+    def test_basic_solution(self):
+        self.assertEqual(solve_math24([3, 3, 8, 8]), '8 / (3 - (8 / 3))')
+
+    def test_no_solution(self):
+        self.assertEqual(solve_math24([1, 1, 1, 1]), 'No solution possible')
+
+    def test_decimal_input(self):
+        self.assertEqual(solve_math24([1.5, 6, 8, 9]), '8 * (3/2 + (9 / 6))')
+
+if __name__ == '__main__':
+    unittest.main()
